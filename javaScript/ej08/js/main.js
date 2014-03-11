c805aaf7824a0cca3172063b3fb48ae377ae4d60
JesusIglesiasNeira <jesusigne@gmail.com>
2014-03-11 13:08:30 +0100

    Ejercicio DOM y Atrib XHTML

---
+++ javaScript/ej08/js/main.js
@@ -0,0 +1,40 @@
+﻿var ocultar = (function(){
+    "use strict";
+
+
+    window.$= Element.prototype.$ =function(selector){
+        var that = (this instanceof Element) ? this : document;
+        var elems = that.querySelectorAll(selector);
+        return(elems.length === 1) ? elems[0] : elems;
+    };
+
+    var enlaces = document.getElementsByTagName("a");
+    console.log(enlaces);
+
+
+
+    var ocultarMostrarTexto=function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        var pulsado = this.previousElementSibling;
+        console.log(pulsado);
+        if (pulsado.className =="oculto"){
+            pulsado.classList.add("visible");
+            pulsado.classList.remove("oculto");
+        }
+        else {
+            pulsado.classList.add("oculto");
+            pulsado.classList.remove("visible");
+        }
+
+    };
+
+       for (var i = enlaces.length-1; i>=0; i--){
+        enlaces[i].addEventListener("click", ocultarMostrarTexto);
+    }
+
+})();
+
+
+
+
