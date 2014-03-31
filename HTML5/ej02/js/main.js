4489573c98eff647ed89b744398a8a9fb62b3f42
Jesus <daw@daw-VirtualBox.(none)>
2014-03-31 18:29:08 +0200

    HTML5 Atributos Globales

---
+++ HTML5/ej02/js/main.js
@@ -0,0 +1,27 @@
+﻿(function(){
+    "use strict";
+    $("#editable").designMode= 'on';
+
+
+
+    var ponerNegrita = function(){
+        document.execCommand("bold",false,null);
+    };
+    var ponerSubrayado = function(){
+        document.execCommand("underline",false,null);
+    };
+    var ponerCursiva = function(){
+        document.execCommand("italic",false,null);
+    };
+    var guardar = function(){
+        localStorage.setItem('contenido', $('#texto').html());
+    };
+
+    $('#texto').html(localStorage.getItem('contenido')|| "Texto editable");
+
+    $(document).on('click','#negrita',ponerNegrita);
+    $(document).on('click','#subrayado',ponerSubrayado);
+    $(document).on('click','#cursiva',ponerCursiva);
+    $(document).on('click','#guardar',guardar);
+
+})();
