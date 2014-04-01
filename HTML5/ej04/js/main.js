f786da74522aca72b2c30bc92ef015f414ed129c
Jesus <daw@daw-VirtualBox.(none)>
2014-04-01 19:24:58 +0200

    ejercicios HTML5

--- HTML5/ej04/js/main.js
+++ HTML5/ej04/js/main.js
@@ -8,7 +8,7 @@
         $nominput= $tipos[i].attributes[0].value;
         var $moder= "Modernizr.inputtypes."+$nominput;
         console.log($moder);
-        if ($moder.value) {
+        if ($moder) {
              $carga.push($nominput);
         } else {
             $nocarga.push($nominput);
@@ -40,3 +40,41 @@
         $('section').append('<p>'+$nocargados+'</p>');
     }
 })();
+/*
+$(function(){
+    'use strict';
+
+        var $progress = $('#progress');
+
+    $(document).on('change', 'input', function(e){
+        var $this = $(this);
+        if ($this.value!== '')
+            $progress.val($progress.val()+1);
+
+    });
+
+
+    //Sacar por pantalla que inputs reconoce el navegador
+console.log(Modernizr);
+    var $ul = $('#datos');
+    $ul.empty();
+    $ul.append('<li>TIPOS: </li>');
+
+    for (var tipo in Modernizr.inputtypes){
+
+        $ul.append('<li>Tipo: ' + tipo + ' ----  Valor: ' + Modernizr.inputtypes[tipo] +'</li>');
+
+    }
+    $ul.append('<li>VIDEO: </li>');
+    for (var codec in Modernizr.video){
+
+        $ul.append('<li>Codec: ' + codec + ' ----  Valor: ' + Modernizr.video[codec] +'</li>');
+
+    }
+    Modernizr.load({
+        test:Modernizr.inputtypes.date,
+        nope:'js/datepolyfill.js'
+    });
+
+});
+*/
