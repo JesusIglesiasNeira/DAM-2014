7de638dc40637aa3712d49a78c477467fd2f1de6
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-01 23:36:22 +0200

    Ejercicios modernizr

--- HTML5/ej05/js/main.js
+++ HTML5/ej05/js/main.js
@@ -20,14 +20,23 @@ console.log(Modernizr);
     for (var tipo in Modernizr.inputtypes){
 
         $ul.append('<li>Tipo: ' + tipo + ' ----  Valor: ' + Modernizr.inputtypes[tipo] +'</li>');
-
     }
+
     $ul.append('<li>VIDEO: </li>');
     for (var codec in Modernizr.video){
 
         $ul.append('<li>Codec: ' + codec + ' ----  Valor: ' + Modernizr.video[codec] +'</li>');
-
     }
 
+    $ul.append('<li>Placeholder: </li>');
+    $ul.append('<li>Codec: placeholder ----  Valor: ' + Modernizr.input.placeholder +'</li>');
+    $ul.append('<li>Progress: </li>');
+    $ul.append('<li>Codec: Progress ----  Valor: ' + Modernizr.progress +'</li>');
+
+
+    Modernizr.load({
+        test:Modernizr.input.color,
+        nope:'libs/vendor/color-polyfill.js'
+    });
 })();
 
