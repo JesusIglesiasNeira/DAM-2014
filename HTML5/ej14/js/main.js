cc12cad8e1bcee67c9e57e5594b7cd9ad17d1e21
Jesus <daw@daw-VirtualBox.(none)>
2014-04-03 19:17:07 +0200

    HTML5 WebStorage

---
+++ HTML5/ej14/js/main.js
@@ -0,0 +1,30 @@
+﻿(function(){
+    "use strict";
+    //$("#editable").designMode= 'on';
+
+
+
+
+    var guardar = function(){
+        sessionStorage.setItem('contenido', $('#texto').val());
+        localStorage.setItem('contenido', $('#texto').val());
+    };
+
+    $('#texto').val(localStorage.getItem('contenido'));
+
+    $(document).on('keyup','#texto',guardar);
+
+    //Eventos para los que estan escuchando
+    function handleStorage(event){
+        if (event.newValue === null) {
+             $('#texto').val("");
+        }
+        else{
+             $('#texto').val(localStorage.getItem('contenido'));
+        }
+    }
+    window.addEventListener('storage',handleStorage,false);
+    window.attachEvent('onstorage',handleStorage);
+
+
+})();
