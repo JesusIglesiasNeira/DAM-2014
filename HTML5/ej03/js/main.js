698b7197c33ec33ac8e32759122be26c6f7c1b8c
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-05 16:31:04 +0200

    HTML5 ejercicio Formulario

--- HTML5/ej03/js/main.js
+++ HTML5/ej03/js/main.js
@@ -4,8 +4,9 @@
         var $inp= $('input');
         var $porcent = parseInt($('meter')[0].attributes[0].value);
         $porcent=0;
-        for (var i= 0; i<= $inp.length-7; i++){
-            if ($inp[i].value.length > 0){
+        for (var i= 0; i<= $inp.length -1; i++){
+            //if ($inp[i].value.length > 0){
+            if ($inp[i].validity.valid){
                 $porcent=$porcent+1;
                 $('meter')[0].attributes[0].value= ($porcent+1).toString();
             }
