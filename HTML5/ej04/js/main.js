24c15fa0e1aa3443b4974ccca53ff256ab97a441
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-05 16:34:14 +0200

    HTML5 ejercicio Modernizr

--- HTML5/ej04/js/main.js
+++ HTML5/ej04/js/main.js
@@ -1,33 +1,45 @@
 ﻿(function(){
     'use strict';
 
-        var $progress = $('#progress');
-
-    $(document).on('change', 'input', function(e){
-        var $this = $(this);
-        if ($this.value!== '')
-            $progress.val($progress.val()+1);
-
-    });
+    var actualizaMetter= function(){
+         var $inp= $('input');
+         var $porcent = parseInt($('meter')[0].attributes[0].value);
+         $porcent=0;
+         for (var i= 0; i<= $inp.length -1; i++){
+             //if ($inp[i].value.length > 0){
+             if ($inp[i].validity.valid){
+                 $porcent=$porcent+1;
+                 $('meter')[0].attributes[0].value= ($porcent+1).toString();
+             }
+         }
+    };
+     $('input').on('blur', actualizaMetter);
 
 
     //Sacar por pantalla que inputs reconoce el navegador
     console.log(Modernizr);
     var $ul = $('#datos');
     $ul.empty();
+
+    //Comprobar inputs soportados
     $ul.append('<li>TIPOS: </li>');
-
     for (var tipo in Modernizr.inputtypes){
-
         $ul.append('<li>Tipo: ' + tipo + ' ----  Valor: ' + Modernizr.inputtypes[tipo] +'</li>');
-
     }
+
+    //Comprobar codecs de video soportados
     $ul.append('<li>VIDEO: </li>');
     for (var codec in Modernizr.video){
-
         $ul.append('<li>Codec: ' + codec + ' ----  Valor: ' + Modernizr.video[codec] +'</li>');
-
     }
 
+    //Comprobar sistemas de almacenamiento local soportados
+    $ul.append('<li>ALMACENAMIENTO: </li>');
+    $ul.append('<li>Sistema: localstorage ----  Valor: ' + Modernizr.localstorage +'</li>');
+    $ul.append('<li>Sistema: sessionstorage ----  Valor: ' + Modernizr.sessionstorage +'</li>');
+    $ul.append('<li>Sistema: websockets ----  Valor: ' + Modernizr.websockets +'</li>');
+    $ul.append('<li>Sistema: websqldatabase ----  Valor: ' + Modernizr.websqldatabase +'</li>');
+    $ul.append('<li>Sistema: webworkers ----  Valor: ' + Modernizr.webworkers +'</li>');
+    $ul.append('<li>Sistema: indexeddb ----  Valor: ' + Modernizr.indexeddb +'</li>');
 })();
 
