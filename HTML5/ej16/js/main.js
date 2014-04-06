d79c2f44f097aa558e3b9af4bd2d1e35599fe4a3
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-06 21:25:40 +0200

    HTML5  ejercicio IndexedDB (sin acabar)

---
+++ HTML5/ej16/js/main.js
@@ -0,0 +1,171 @@
+﻿(function(){
+    "use strict";
+
+
+    /////////////////////////////////////////////////////////////////////////////
+    //Comprobaciones previas a abrir BD:
+    window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+    if ('webkitIndexedDB' in window) {
+        window.IDBTransaction = window.webkitIDBTransaction;
+        window.IDBKeyRange = window.webkitIDBKeyRange;
+    }
+
+    var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+    //Abrir la BBDD e insertar un almacen con 2 objetos
+    var creaBD = function(){
+        var request = indexedDB.open("BDTareas",1);
+        var task=[
+            {index:"1", date:"2010-10-08", completed:false, description:"Recoger la basura"},
+            {index:"2", date:"2010-10-12", completed:true, description:"Limpiar el POLVO"}];
+
+        request.onerror = function(e){
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) {
+            console.log("UPDATING") ;
+            var db = event.target.result;
+            var objectStore = db.createObjectStore("almacenTareas",{keyPath:"index"});
+            objectStore.createIndex("completed","completed",{unique:false});
+            //objectStore.createIndex("completed","index",{unique:true});
+
+            for(var i in task){
+                objectStore.add(task[i]);
+            }
+        };
+        request.onsuccess = function(e) {
+            console.log("life is good:" + e) ;
+        };
+    };
+
+
+    /////////////////////////////////////////////////////////////
+    //Añadir objetos al almacen:
+    var addupdatetask = function(){
+        var request = indexedDB.open("BDTareas",1);
+        var task=[
+            {index:$('#index').val(), date:$('#date').val(), completed:$('#completed')[0].checked,
+            description:$('#description').val()}];
+        request.onerror = function(e){
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) {
+            alert('onUpdateneeded not implemented on addupdatetask');
+
+        };
+        request.onsuccess = function(e) {
+            var db = event.target.result;
+            var trans = db.transaction(['almacenTareas'], "readwrite");
+            var store = trans.objectStore("almacenTareas");
+            var request = store.put(task[0]);
+
+        };
+    };
+
+
+
+
+    //////////////////////////////////////////////////////////////////////////////
+   // Eliminar todos los objetos del almacén  /////////////////////NO FUNCIONAAAAAAAAAA///////////////////////////////////
+    var deleteAllTask = function(){
+        var request = indexedDB.open("BDTareas",1);
+        var key= $('#indexrem').val();
+        request.onerror = function(e){
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) {
+            alert('onUpdateneeded not implemented on deleteTask');
+
+        };
+        request.onsuccess = function(e) {
+            var db = event.target.result;
+            var trans = db.transaction(['almacenTareas'], "readwrite");
+            var store = trans.objectStore("almacenTareas");
+            request = store.clear(); //eliminar todos los objetos de un almacen
+        };
+    };
+    //////////////////////////////////////////////////////////////////////////////
+   // Eliminar objeto del almacén
+    var deleteTask = function(){
+        var request = indexedDB.open("BDTareas",1);
+        var key= $('#indexrem').val();
+        request.onerror = function(e){
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) {
+            alert('onUpdateneeded not implemented on deleteTask');
+
+        };
+        request.onsuccess = function(e) {
+            var db = event.target.result;
+            var trans = db.transaction(['almacenTareas'], "readwrite");
+            var store = trans.objectStore("almacenTareas");
+            var request = store.delete(key);
+        };
+    };
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Obtener objetos del almacen:
+    var getTask = function(){
+        var request = indexedDB.open("BDTareas",1);
+        var key= $('#indexrem').checked || false;
+        key = key.toString();
+        request.onerror = function(e){
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) {
+            alert('onUpdateneeded not implemented on deleteTask');
+
+        };
+        request.onsuccess = function(e) {
+            var db = event.target.result;
+            var trans = db.transaction(['almacenTareas'], "readwrite");
+            //var store = trans.objectStore("almacenTareas").index('completed');
+            var store = trans.objectStore("almacenTareas");
+            var data = [];
+            //var keyRange = IDBKeyRange.only(key);
+            //var req = store.openCursor(keyRange);
+            var req = store.openCursor();
+            req.onsuccess = function (event) {      //objetos de un almacen, en lugar de un unico objeto, pasando
+                var cursor = event.target.result;       //como parametro un objeto de tipo IDBKeyRange
+                if (cursor) {
+                    data.push(cursor.value);    // value is the stored object
+                    cursor.continue();   // get the next object
+                }
+                else {//Objects are in data[ ]
+                }
+            console.log(data);
+            };
+        };
+    };
+
+
+
+
+
+    $(document).on('click','#borra',deleteAllTask);
+    $(document).on('click','#addtask',addupdatetask);
+    $(document).on('click','#deletetask',deleteTask);
+    $(document).on('click','#updatetask',addupdatetask);
+    $(document).on('click','#getTask',getTask);
+    $(document).on('click','#create',creaBD);
+    $(document).on('click','#delete',deleteAllTask);
+
+})();
+
+
+/*
+Crear un objeto que encapsule una base de datos IndexedDB, que nos permita acceder
+a una base de datos para añadir, modificar, eliminar y obtener registros. Dicha base de
+datos va a almacenar una sencilla lista de tareas pendientes. Los requisitos son:
+✔ Disponer de un almacén de tareas pendientes. Sus propiedades son: un
+identificador único que actúa como índice, el texto descriptivo, una propiedad que
+nos indique si la tarea está completada o no y la fecha/hora de creación
+✔ Crear un método addTask que dado un objeto que corresponde con una tarea, lo
+almacene en la base de datos
+✔ Crear un método removeTask que dado un identificador de una tarea, lo elimine de
+la base de datos. Éste método debe devolver la eliminada.
+✔ Crear un método updateTask que dado un identificador de una tarea, actualice los
+datos correspondientes a la tarea en la base de datos
+✔ Crear un método getTasks que dado un parámetro booleano completado,
+nos devuelva las tareas que se encuentran completadas o no
+*/
---
+++ HTML5/otros/indexedDBbase.js
@@ -0,0 +1,79 @@
+﻿$(document).ready(function(){
+    "use strict";
+    //Comprobaciones previas a abrir BD:
+    window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB;
+    if ('webkitIndexedDB' in window) {
+        window.IDBTransaction = window.webkitIDBTransaction;
+        window.IDBKeyRange = window.webkitIDBKeyRange;
+    }
+
+    //Abrir BD (si no existe se crea)(las llamadas son asincronas) y Crear almacén:
+    var creaBD = function(){
+        var db = null, version = '0.1';
+        var request = window.indexedDB.open(MiDB);
+        request.onsuccess = function (event) {
+            db = event.target.result; // onsuccess recive un evento con estas propiedades.
+            if (version != db.version) { // Necesario control de versiones:
+                var verRequest = db.setVersion(version);  // set the version to 0.1
+                verRequest.onsuccess = function (event) {
+                    var store = db.createObjectStore('miAlmacen', { // crear almacen Objetos
+                        keyPath: 'title',           // clave única xa los objetos del almacen
+                        autoIncrement: false        // autoincrementada
+                    });
+                };
+                verRequest.onerror = function () {
+                    alert('unable to set the version :' + version);
+                };
+            }
+        };
+        request.onerror = function (event) { //En IndexedDB, los errores que se producen escalan hasta el objeto request
+            alert('Something failed: ' + event.target.message);
+        };
+        request.onupgradeneeded = function(event) { // unico lugar que deberiamos poner las consultas que
+            var db = event.target.result;   //modifiquen la estructura de la base de datos
+            // Create object store
+        };
+    };
+
+    /////////////////////////////////////////////////////////////
+    //Añadir objetos al almacen:
+    var anadeObj = function(){
+        var video = {
+            title: "Belly Dance Bruce - Final Strike",
+            date: (new Date()).getTime(),
+            director: "Bruce Awesome",
+            length: 169, // in minutes
+            rating: 10,
+            cover: "/images/wobble.jpg"
+        };
+        var myIDBTransaction = window.IDBTransaction || window.webkitIDBTransaction  || { READ_WRITE: 'readwrite' };
+        var transaction = db.transaction(['miAlmacen'], myIDBTransaction.READ_WRITE); //Crea una nueva                          //transaccion de lectura/escritura, sobre los almacenes indicados
+        var store = transaction.objectStore('miAlmacen'); // Obtiene el almacen de objetos sobre el que queremos realizar las
+                                //operaciones
+        //1ºopc) var request = store.add(video); //add: anade un nuevo objeto al almacen. Es obligatorio que los nuevos
+                                                // datos no existan en el almacen, si no provocaria un ConstraintError
+        var request = store.put(video);// put: actualiza el valor del objeto si existe, o lo anade si no existe en el almacen.
+    };
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Obtener objetos del almacen:
+    var obtenObj = function(){
+        var myIDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || { READ: 'read' };
+        var key = "Belly Dance Bruce - Final Strike";
+        var transaction = db.transaction(['miAlmacen'], myIDBTransaction.READ);
+        var store = transaction.objectStore('miAlmacen');
+        //1ºopc)var request = store.get(key); //Obtiene 1 elemento. Si no existe el obj devuelve null
+        var data = [ ];
+        var request = store.openCursor();//Es recomendable utilizar el metodo “openCursor()” para que si el objeto no existe, el
+        // var singleKeyRange = IDBKeyRange.only("Belly Dance Bruce - Final Strike"); //valor del resultado sea null
+        // var request = store.openCursor(singleKeyRange);  //El  metodo “openCursor” tambien permitira obtener todos los
+        request.onsuccess = function (event) {      //objetos de un almacen, en lugar de un unico objeto, pasando
+            var cursor = event.target.result;       //como parametro un objeto de tipo IDBKeyRange
+            if (cursor) {
+                data.push(cursor.value);    // value is the stored object
+                cursor.continue();   // get the next object
+            } else {//Objects are in data[ ]}
+        }
+    };
+
+});
