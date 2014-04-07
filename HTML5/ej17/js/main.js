51fa7f615f08e10041a6c1de3129594712588a83
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-07 12:56:21 +0200

    HTML5 ejercicio WebWorkers (nums primos)

---
+++ HTML5/ej17/js/main.js
@@ -0,0 +1,27 @@
+﻿(function(){
+    "use strict";
+
+
+    var escribenum= function(evt){
+        var nums=  $('#primos').text();
+        $('#primos').text(nums +" "+ evt.data+" ");
+    };
+
+    var obtenPrimos = function(){
+        if(Modernizr.webworkers) {
+            var number=  $('#num').val();
+            if (number){
+                var worker = new Worker('js/worker-primos.js');
+                //calculaprimos(number);
+                worker.postMessage({'num': number});
+                worker.addEventListener('message', escribenum, false);
+            }
+        }
+        else {
+            alert('El explorador NO soporta Web workers');
+        }
+    };
+
+    $(document).on('click','#obtprim',obtenPrimos);
+
+})();
---
+++ HTML5/ej17/js/worker-primos.js
@@ -0,0 +1,24 @@
+﻿this.addEventListener('message', function(e) {
+    var num = parseInt(e.data.num);
+    var primo = true;
+    if (num && num == 1){
+        postMessage(num);
+    }
+    else if(num && num == 2){
+        postMessage(1);
+        postMessage(2);
+    }
+    else{
+        postMessage(1);
+        for (var n=2; n<=num; n++){
+            primo = true;
+            for (var i=2; i<n; i++){
+                if ((n%i)== 0){
+                    primo = false;
+                }
+            }
+            if (primo){postMessage(n);}
+        }
+    }
+
+}, false);
