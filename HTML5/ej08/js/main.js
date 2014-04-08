25b83eac3acd4001a66d38b409f158c17e15316f
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-08 12:14:38 +0200

    HTML5 ejercicio Canvas

--- HTML5/ej08/js/main.js
+++ HTML5/ej08/js/main.js
@@ -18,52 +18,55 @@
             ctx2.strokeRect(50,50,50,50);
             ctx2.fillRect(25,25,100,100);
             ctx2.clearRect(45,45,60,60);
-
-            ctx2.beginPath();
-            ctx2.moveTo(25,25); // Punto inicial para dibujar
-            ctx2.lineTo(105,25);
-            ctx2.lineTo(25,105);
-            ctx2.closePath();
-            ctx2.stroke();
         }
-
         canvas = document.getElementById("canvas3");
         if (canvas.getContext) {
             var ctx3 = canvas.getContext("2d");
+            ctx3.beginPath();
+            ctx3.moveTo(25,25); // Punto inicial para dibujar
+            ctx3.lineTo(105,25);
+            ctx3.lineTo(25,105);
+            ctx3.closePath();
+            ctx3.stroke();
+        }
+
+        canvas = document.getElementById("canvas4");
+        if (canvas.getContext) {
+            var ctx4 = canvas.getContext("2d");
 
             for(var i=0;i<4;i++){
                 for(var j=0;j<3;j++){
-                    ctx3.beginPath();
+                    ctx4.beginPath();
                     var x              = 25+j*50;       // coordenada x
                     var y              = 25+i*50;       // coordenada y
                     var radius         = 20;            // radio del arco
                     var startAngle     = 0;             // punto inicial del círculo
                     var endAngle       = Math.PI+(Math.PI*j)/2; // punto final
                     var anticlockwise  = i%2==0 ? false : true;
-                    ctx3.arc(x,y,radius,startAngle,endAngle, anticlockwise);
-                    if (i>1) ctx3.fill();
-                    else ctx3.stroke();
+                    ctx4.arc(x,y,radius,startAngle,endAngle, anticlockwise);
+                    if (i>1) ctx4.fill();
+                    else ctx4.stroke();
                 }
             }
         }
 
-        canvas = document.getElementById("canvas4");
+        canvas = document.getElementById("canvas5");
             if (canvas.getContext) {
-                var ctx4 = canvas.getContext("2d");
+                var ctx5 = canvas.getContext("2d");
 
                 for (var k=0;k<6;k++){
                     for (var l=0;l<6;l++){
-                        ctx4.fillStyle = 'rgb(' + Math.floor(255-42.5*k) + ',' + Math.floor(255-42.5*l) + ',0)';
-                            ctx4.fillRect(l*25,k*25,25,25);
+                        ctx5.fillStyle = 'rgb(' + Math.floor(255-42.5*k) + ',' + Math.floor(255-42.5*l) + ',0)';
+                            ctx5.fillRect(l*25,k*25,25,25);
                     }
                 }
             }
 
-        canvas = document.getElementById("canvas5");
+        canvas = document.getElementById("canvas6");
             if (canvas.getContext) {
-                var ctx5 = canvas.getContext("2d");
-                var gradient = ctx5.createLinearGradient(0, 0, 0, canvas.height);
-                gradient = ctx5.createRadialGradient(canvas.width/2,
+                var ctx6 = canvas.getContext("2d");
+                var gradient = ctx6.createLinearGradient(0, 0, 0, canvas.height);
+                gradient = ctx6.createRadialGradient(canvas.width/2,
                                         canvas.height/2,
                                         0,
                                         canvas.width/2,
@@ -71,30 +74,30 @@
                                         150);
                     gradient.addColorStop(0, '#fff');
                     gradient.addColorStop(1, '#000');
-                    ctx5.fillStyle = gradient;
-                    ctx5.fillRect(0, 0, canvas.width, canvas.height);
+                    ctx6.fillStyle = gradient;
+                    ctx6.fillRect(0, 0, canvas.width, canvas.height);
             }
 
 
-        canvas = document.getElementById("canvas6");
+        canvas = document.getElementById("canvas7");
         if (canvas.getContext) {
-            var ctx6 = canvas.getContext("2d");
+            var ctx7 = canvas.getContext("2d");
 
-            ctx6.globalAlpha = 0.2;
+            ctx7.globalAlpha = 0.2;
             // Dibujar círculos semitransparentes
             for (var m=0;m<7;m++){
-                ctx6.beginPath();
-                ctx6.arc(75,75,10+10*m,0,Math.PI*2,true);
-                ctx6.fill();
+                ctx7.beginPath();
+                ctx7.arc(75,75,10+10*m,0,Math.PI*2,true);
+                ctx7.fill();
             }
-            ctx6.translate(100, 20);
+            ctx7.translate(100, 20);
 
             var radians = 45 * Math.PI / 180;
-            ctx6.rotate(radians);
+            ctx7.rotate(radians);
             for (var n=0;n<7;n++){
-                ctx6.beginPath();
-                ctx6.arc(75,75,10+10*n,0,Math.PI*2,true);
-                ctx6.fill();
+                ctx7.beginPath();
+                ctx7.arc(75,75,10+10*n,0,Math.PI*2,true);
+                ctx7.fill();
             }
 
         }
