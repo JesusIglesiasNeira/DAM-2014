5bffada63b3d2fe31ddefde9820a6458b9635bd1
JesusIglesiasNeira <jesusigne@gmail.com>
2014-04-13 17:22:27 +0200

    HTML5 ejercicios WebSQL

--- HTML5/ej15/js/main.js
+++ HTML5/ej15/js/main.js
@@ -13,28 +13,27 @@
 
     var addtweet= function(){
         var nomb = $('#name').val();
+        var idtweet = parseInt(new Date().getTime());
         var fecha = $('#fecha').val();
         var texto = $('#texto').val();
         var usu = nomb[0]+nomb[1]+nomb[2]+nomb[0];
         var now = new Date();
         var idus = nomb[0]+now.getTime().toString()+texto[0]+nomb[0];
-        var tweets = [{id:idus,user:usu, date:fecha,text:texto}];
-        var usuarios= [{user:usu, name:nomb}];
-        guardaUsers(usuarios);
+        var tweets = [{"created_at":fecha,"id":idtweet,"text":texto,"user":{"id":idus,"name":nomb,"screen_name":usu,"created_at":fecha}}];
         guardaTweets(tweets);
     };
     var deletetweet= function(){
-        var idtweet =  $('#elimin').val();
+        var idtweet =   $('#elimin').val();
         if (idtweet){
-            db.transaction(function (tx) { //CADA TWEET EN UNA TRANSACCION → Procesa todas
-                tx.executeSql('DELETE FROM tweets WHERE id='+"'"+idtweet+"'");
+            db.transaction(function (tx) {
+                tx.executeSql('DELETE FROM tweets WHERE id="'+idtweet+'"');
             });
         }
         else{alert('Introduzca un id de tweet válido');}
     };
 
     var updatetweet = function(){
-        var idtweet =  $('#actualid').val();
+        var idtweet = $('#actualid').val();
         var texto = $('#actualtext').val();
         if (idtweet){
             db.transaction(function (tx) {
@@ -47,16 +46,19 @@
     var obtentweetposterior = function(){
         var fecha =  $('#fechapost').val();
         var $ul = $('#tweetfecha');
+        $ul.empty();
         if (fecha){
             db.transaction(function (tx) {
-                tx.executeSql("SELECT date,text,users.name AS usu FROM tweets INNER JOIN users ON tweets.user = users.user  AND tweets.date >= '"+fecha+"'",[],
+                tx.executeSql("SELECT tweets.id,tweets.date,tweets.text,users.name AS usu FROM tweets INNER JOIN users ON tweets.iduser = users.idusu  AND tweets.date >= '"+fecha+"'",[],
                     function callback(tx, results) {
                     var len = results.rows.length, i;
                         for (i = 0; i < len; i++) {
                             $ul.append('<li>'+
-                                " Usuario: "+results.rows.item(i).usu+
-                                " Texto: "+results.rows.item(i).text+
-                                " Fecha: "+results.rows.item(i).date+
+                                "<strong> Usuario: </strong>"+results.rows.item(i).usu+
+                                "<strong> Fecha: </strong>"+results.rows.item(i).date+
+                                "<strong> idTweet: </strong>"+results.rows.item(i).id+"<br/>"+
+                                "<strong> Texto: </strong><br/>"+results.rows.item(i).text+"<br/>"+
+                                "---------------------------------------------------------------------------------------------------------------------------------"+
                                 '</li>');
                         }
                     },
@@ -73,21 +75,27 @@
 
     //insertar en la BBDD cada tweet
     var guardaTweets = function(tweets){
+        console.log(tweets[0]);
+        guardaUsers(tweets);
          $.each(tweets, function(tweet) {
+            var id = tweets[tweet].id.toString();
+            var idusr = tweets[tweet].user.id.toString();
             db.transaction(function (tx) { //CADA TWEET EN UNA TRANSACCION → Procesa todas
                 //var time = (new Date(Date.parse(tweets[tweet].created_at))).getTime();
-                tx.executeSql('INSERT INTO tweets (id, user, date, text) VALUES (?, ?, ?, ?)',
-                [tweets[tweet].id, tweets[tweet].user, tweets[tweet].date, tweets[tweet].text]);
+                tx.executeSql('INSERT INTO tweets (id, iduser, date, text) VALUES (?, ?, ?, ?)',
+                [id, idusr, tweets[tweet].created_at, tweets[tweet].text]);
             });
         });
     };
 
     var guardaUsers = function(users){
          $.each(users, function(user) {
-            db.transaction(function (tx) { //CADA TWEET EN UNA TRANSACCION → Procesa todas
+
+            db.transaction(function (tx) {
+                var idusr = users[user].user.id.toString();
                 //var time = (new Date(Date.parse(tweets[tweet].created_at))).getTime();
-                tx.executeSql('INSERT INTO users (user, name) VALUES (?, ?)',
-                [users[user].user, users[user].name]);
+                tx.executeSql('INSERT INTO users (idusu, user, name, description, location, createdAt) VALUES (?, ?, ?, ?, ?, ?)',
+                [idusr, users[user].user.screen_name, users[user].user.name, users[user].user.description, users[user].user.location, users[user].user.created_at]);
             });
         });
     };
@@ -96,14 +104,15 @@
     //funcion que obtiene los tweets e inseta en la tabla creada los datos de cada tweet
     var getTweets =function () {
         var tweets = $.ajax({
-            url : 'servidor/cargaTweets1.json',
+            url : 'servidor/search.json',
             type : 'POST',
             //data : { user : usuario },
             dataType : 'JSON',
             cache : false,
             success : function(data){
                 //var lis = json.parse(data);
-                guardaTweets(data);
+                guardaTweets(data.statuses);
+                console.log(data.statuses);
             },
             error : function(jqXHR, textStatus, errorThrow){
                 //alert(errorThrow);
@@ -112,37 +121,19 @@
         });
     };
 
-    var getUser =function () {
-        var tweets = $.ajax({
-            url : 'servidor/cargaUsers1.json',
-            type : 'POST',
-            //data : { user : usuario },
-            dataType : 'JSON',
-            cache : false,
-            success : function(data){
-                //var lis = json.parse(data);
-                guardaUsers(data);
-            },
-            error : function(jqXHR, textStatus, errorThrow){
-                //alert(errorThrow);
-                console.log(errorThrow);
-            }
-        });
-    };
 
 
 
     //Crear las tablas
     var createTable= function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS users(user PRIMARY KEY, name)', []);
-        tx.executeSql('CREATE TABLE IF NOT EXISTS tweets(id PRIMARY KEY, user, date, text, FOREIGN KEY(user) REFERENCES users(user))', []);
+        tx.executeSql('CREATE TABLE IF NOT EXISTS users(idusu PRIMARY KEY, user, name, description, location, createdAt)', []);
+        tx.executeSql('CREATE TABLE IF NOT EXISTS tweets(id PRIMARY KEY, iduser, date, text, FOREIGN KEY(iduser) REFERENCES users(idusu))', []);
     };
 
     //Crear y definir la BBDD
     var db = openDatabase('tweetdb', '1.0', 'All my tweets', 2 * 1024 * 1024);
     if(db){
         db.transaction(createTable);
-        getUser();
         getTweets();
     }
 
@@ -182,4 +173,83 @@ Obtener los últimos 25 tweets que tienen como hashtag #html5 de la siguiente co
  http://search.twitter.com/search.json?q=%23html5&rpp=25&result_type="recent"
 
 Consultad la API de Twitter para identificar el formato del resultado, nombres de campos, etc.
+*/
+/*
+    //insertar en la BBDD cada tweet
+    var guardaTweets = function(tweets){
+        guardaUsers(tweets);
+         $.each(tweets, function(tweet) {
+            db.transaction(function (tx) { //CADA TWEET EN UNA TRANSACCION → Procesa todas
+                //var time = (new Date(Date.parse(tweets[tweet].created_at))).getTime();
+                tx.executeSql('INSERT INTO tweets (id, iduser, date, text) VALUES (?, ?, ?, ?)',
+                [tweets[tweet].id, tweets[tweet].user.id, tweets[tweet].created_at, tweets[tweet].text]);
+            });
+        });
+    };
+
+    var guardaUsers = function(users){
+         $.each(users, function(user) {
+
+            db.transaction(function (tx) { //CADA TWEET EN UNA TRANSACCION → Procesa todas
+                //var time = (new Date(Date.parse(tweets[tweet].created_at))).getTime();
+                tx.executeSql('INSERT INTO users (idusu, user, name, description, location, createdAt) VALUES (?, ?)',
+                [users[user].user.id, users[user].user.screen_name, users[user].user.name, users[user].user.description, users[user].user.location, users[user].user.created_at]);
+            });
+        });
+    };
+
+
+    //funcion que obtiene los tweets e inseta en la tabla creada los datos de cada tweet
+    var getTweets =function () {
+        var tweets = $.ajax({
+            url : 'servidor/search.json',
+            type : 'POST',
+            //data : { user : usuario },
+            dataType : 'JSON',
+            cache : false,
+            success : function(data){
+                //var lis = json.parse(data);
+                guardaTweets(data.statuses);
+                console.log(data.statuses);
+            },
+            error : function(jqXHR, textStatus, errorThrow){
+                //alert(errorThrow);
+                console.log(errorThrow);
+            }
+        });
+    };
+
+    var getUser =function () {
+        var users = $.ajax({
+            url : 'servidor/cargaUsers1.json',
+            type : 'POST',
+            //data : { user : usuario },
+            dataType : 'JSON',
+            cache : false,
+            success : function(data){
+                //var lis = json.parse(data);
+                guardaUsers(data);
+            },
+            error : function(jqXHR, textStatus, errorThrow){
+                //alert(errorThrow);
+                console.log(errorThrow);
+            }
+        });
+    };
+
+
+
+    //Crear las tablas
+    var createTable= function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS users(idusu PRIMARY KEY, user, name, description, location, createdAt)', []);
+        tx.executeSql('CREATE TABLE IF NOT EXISTS tweets(id PRIMARY KEY, iduser, date, text, FOREIGN KEY(iduser) REFERENCES users(idusu))', []);
+    };
+
+    //Crear y definir la BBDD
+    var db = openDatabase('tweetdb', '1.0', 'All my tweets', 2 * 1024 * 1024);
+    if(db){
+        db.transaction(createTable);
+        //getUser();
+        getTweets();
+    }
 */
