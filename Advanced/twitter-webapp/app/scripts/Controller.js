a0df830956dae52a21b241bf6f4e2fd914957640
Jesus <daw@daw-VirtualBox.(none)>
2014-04-22 19:29:02 +0200

    Ejercicios Advanced

---
+++ Advanced/twitter-webapp/app/scripts/Controller.js
@@ -0,0 +1,44 @@
+﻿define('Controller',['Data','Service'],function(DB, srv){
+    'use strict';
+
+    var processtweets = function(data){
+        var tweets =[];
+        var tweet ={
+            id:'',
+            text:'',
+            usr:'',
+            date:''
+        };
+
+        if (data && data.statuses && data.statuses.length >0){
+            for (var i in data.statuses){
+                tweet ={id:'',text:'',usr:'',date:''};
+                tweet.id = data.statuses[i].id_str;
+                tweet.text = data.statuses[i].text;
+                tweet.usr = data.statuses[i].user.name;
+                tweet.date = new Date(data.statuses[i].created_at).getTime();
+
+                tweets.push(tweet);
+            }
+            DB.addTweets(tweets,
+                function(){console.log('conseguido');},//success
+                function(){console.log('no conseguido');} //error
+
+                );
+        }
+
+    };
+
+    var error = function(){
+
+    };
+
+    var getTweetsFromTwitter = function(){
+        srv.getTweets({}, processtweets, error);
+    };
+
+    return{
+        getTweetsFromTwitter:getTweetsFromTwitter
+    };
+
+});
---
+++ Advanced/twitter-webapp/test/spec/testController.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    require.config({
+        baseUrl : '../app/scripts/',
+        paths : {
+            // pouchdb  : '../bower_components/pouchdb/dist/pouchdb-nightly',
+            jquery : '../bower_components/jquery/dist/jquery',
+        },
+        shim : {
+        }
+    });
+
+
+    describe('Controller module', function () {
+        var ctrl;
+        var srv;
+        var DB;
+
+        beforeEach(function(done){
+            require(['Controller', 'Service','Data'], function(Controller, Service, Data){
+                ctrl = Controller;
+                srv = Service;
+                DB = Data;
+                sinon.spy(srv,'getTweets');
+                sinon.spy(DB,'addTweets');
+                done();
+            });
+        });
+
+        /*afterEach(function(done){
+            $.ajax.restore();
+            done();
+        });*/
+
+
+        describe('#getTweetsFromTweeter', function () {
+            it('Get all tweets from Twiter ', function (done) {
+                ctrl.getTweetsFromTwitter();
+                assert.isTrue(srv.getTweets.calledOnce,'getTweets not called');
+                assert.isTrue(DB.addTweets.calledOnce,'setTweets not called');
+
+            });
+        });
+
+
+    });
+})();
